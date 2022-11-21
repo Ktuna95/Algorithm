fe9635ecc46449336163131f306d10f31d726a4b
Ktuna <62793010+Ktuna95@users.noreply.github.com>
2022-11-21 18:49:31 +0900

    [level 1] Title: 폰켓몬, Time: 2.04 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,23 +1,22 @@
 /*
-    일차원배열 T/F 만들기 false로 기본값설정
-    입력받은 요소의 값이 들어오면 일차원배열의 해당값의 요소를 true로 변경
+    일차원배열을 false로 선언 및 초기화
+    입력값이 들어 올때 마다
+    일차원배열의 요소를 true로 변경
     이미 true라면 입력받은 요소를 다시 순회
+    
+    받은 포켓몬이 최대 횟수로 도달하면 반복문 탈출
 */
 function solution(nums) {
-    const tfArr = new Array(200000).fill(false);
-    let answer = 0; //다른 종류의 포켓몬을 받은 횟수
-    let cnt = 0; //
-    let max = nums.length / 2;
+    const tfArr = new Array(200000).fill(false); //포켓몬의 종류는 20만마리
+    let answer = 0;                              //다른 종류의 포켓몬을 받은 횟수    
+    let max = nums.length / 2;                   //최대 받을 수 있는 횟수
 
     for(const pokemon of nums){
         if(tfArr[pokemon] === false){
-            tfArr[pokemon] = true;            
-            cnt++;            
+            tfArr[pokemon] = true;                           
             answer++;    
-            
-            //continue;
         }        
-        if (cnt === max) break;
+        if (answer === max) break;
     }    
 
     return answer;
