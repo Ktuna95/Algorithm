46d04094a27e61b3b50af8d9581544ef1ae6507a
Ktuna <62793010+Ktuna95@users.noreply.github.com>
2022-11-30 14:42:21 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -8,32 +8,35 @@
   여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 
   체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
 
-  1. reserve배열요소의 값이 lost배열요소값 -1, +1범위에 있는지
-   1-1. 있다면 reserve배열요소와 lost 배열요소 삭제 (체육복 빌려줌)
-  2. 없다면 lost배열요소 삭제 후 answer++
+  1. sort
+  
+  2. 여분을 가진 학생 중 도둑맞은 학생 삭제
+  
+  3. reserve배열요소의 값이 lost배열요소값 -1, +1범위에 있는지 확인
+   3-1. 있다면 shift로 두 배열 요소 제거
+   
+  4. 범위에 값이 없다면 reserve배열 0번째 값이 lost 배열 0번째 값보다 크다면 
+     lost제거 후 반복문 다시실행
+  
   3. 반복문이 끝나면 전체학생수(n) - 체육복 없는학생수(answer)
  */
+
 var answer = 0;    
 
-function calculate(n, lost, filted_reserve){
-    while(filted_reserve.length !== 0){
-        if(filted_reserve[0] === lost[0]-1 || filted_reserve[0] === lost[0]+1) {            
-            filted_reserve.shift();
+function calculate(n, lost, reserve){
+    while(reserve.length !== 0){
+        if(reserve[0] === lost[0]-1 || reserve[0] === lost[0]+1) { 
+            reserve.shift();
             lost.shift();
         }
         else{
-            if(filted_reserve[0]>lost[0]) lost.shift();            
+            if(reserve[0]>lost[0]) lost.shift();            
             else {
-                filted_reserve.shift();
+                reserve.shift();
                 answer++;
-            }
-            
+            }            
         }
     }
-    // while(filted_reserve.length < lost.length){
-    //     lost.shift();
-    //     answer++;
-    // }
     return n - answer ;
 }  
 
@@ -42,14 +45,13 @@ function solution(n, lost, reserve) {
     lost.sort((a,b)=>a-b);
     reserve.sort((a,b)=>a-b);
 
-    //예외처리) 여분을 가진 학생 중 도둑맞은 학생 제외한 값 filted
+    //예외처리) 여분을 가진 학생 중 도둑맞은 학생
     let filted_reserve = lost.filter(same => !reserve.includes(same));
     let filted_lost = reserve.filter(same => !lost.includes(same));
-
-    if(filted_reserve.length === 0) return calculate(n, lost, reserve)
-    else return calculate(n, filted_lost, filted_reserve)
+    
+    return calculate(n, filted_lost, filted_reserve)
 
 }
-solution(5,[2,4], [1,3,5]) //5
+//solution(5,[2,4], [1,3,5]) //5
 //solution(5,[1,2], [2,3]) //4
 //solution(5,[5,4,2],[2,4]); //4
