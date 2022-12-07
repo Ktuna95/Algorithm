6a9b740d7e85ec33dae7d77d39c0b4a0701c15cf
Ktuna <62793010+Ktuna95@users.noreply.github.com>
2022-12-07 18:54:19 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.20 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -5,9 +5,11 @@ let answer = 0;
         // number별 약수 개수 구하기
         let count = 0;
         for(let i = 1; (i*i) <= num; i++) {
-            if(num % i == 0) {
+            // 약수 구하는 방법 
+            // 1) 나머지 연산(%) 시 결과 값이 0일 때 (몫이 정수로 나누어 떨어지는 수)
+            if(num % i === 0) {
                 let share = Number((num / i));
-                if((share*share) == num) {
+                if((share*share) === num) {
                     count += 1;
                 } else {
                     count += 2;
@@ -15,7 +17,7 @@ let answer = 0;
             }
         }
  
-        // 약수 개수가 limit 넘는지 비교
+        // 약수 개수가 limit 넘는지 비교 크면 power를 더해주기
         if(count > limit) {
             answer += power;
         } else {
