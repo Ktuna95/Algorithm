8aea3ae97605cd5574e0110820113173c2c81cef
Ktuna <62793010+Ktuna95@users.noreply.github.com>
2022-12-18 18:18:07 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.54 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,41 +1,30 @@
-
 function solution(s) {
     var answer = [];
-    let tmp = s;
-    let round = 0;
-    let delzero = 0;
-    console.log(tmp)
+    let tmp = s;        // 입력값 저장
+    let round = 0;      // 실행한 회차
+    let delzero = 0;    // 제거할 모든 0의 개수    
     
     const take1 = /[1]/g              // s에서 '1'만 가져오기
     const take0 = /[0]/g              // s에서 '0'만 가져오기      
     
-    let s_take0 = 0;
-    let s_take1 = 0;
+    let search0 = 0;    // 제거할 0의 개수
+    let search1 = 0;    // 0제거 후 길이
 
     while(tmp.length !== 1){                    
             round++; // 반복문이 돌 때마다 round를 더해주기
-            //* Step1. 제거할 0의 개수, 0 제거 후 길이 구하기            
+            //* Step1. 제거할 0의 개수 구하기            
+            // tmp에 0이 있다면 0의 개수를 반환
             if(take0.test(tmp)){                     // tmp에 0이 있다면
-                s_take0 = tmp.match(take0).length;   // 0을 0의 개수 반환
+                search0 = tmp.match(take0).length;   // tmp의 0의 개수를 반환                
             }else{
-                s_take0 = 0;
+                search0 = 0; // tmp에 0이 없다면 제거할 0의 개수 0개로 선언
             }
-            
-            s_take1 = tmp.match(take1).length;   // 0제거 후 1의 개수 반환
-            
-            console.log(s_take1)// 제거할 0의 개수
-            console.log(s_take0)// 0제거 후 길이
-            delzero += s_take0; // 0을 제거한 개수
-    
-            //* Step2. 이진 변환 결과
-            tmp = s_take1.toString(2); // 1의 개수를 이진법화
-            console.log(tmp);
-               
-    }
-    console.log(round, delzero)
+            delzero += search0; // 0을 제거한 개수
+            //* Step2. 이진법화 하기위한 1의 개수 구하기
+            search1 = tmp.match(take1).length;   // 1의 개수를 반환               
+            //* Step3. 이진법화로 변환하기 
+            tmp = search1.toString(2); // 1의 개수를 이진법화                           
+    }    
     answer.push(round, delzero);    
     return answer;
 }
-//solution("110010101001");
-solution("01110");
-//solution("1111111");
