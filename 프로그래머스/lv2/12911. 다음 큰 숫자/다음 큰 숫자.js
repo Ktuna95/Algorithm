646994b84574ec87c201a0ad1b688eb4b3341756
Ktuna <62793010+Ktuna95@users.noreply.github.com>
2022-12-23 10:45:46 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -2,8 +2,9 @@
     1.  n < 수
     2.  두 수 모두 이진수 변환시 1의개수 동일 1 2 4 6 8 10
     ㄴ 이진법변환후 split'1'.length
-    3. 어떤수를 1부터 계속 출발하며 n과 비교하기
+    3. 어떤수를 n부터 1씩 증가시키며 이진법변환한 n의 1의개수와 비교하기
 */
+
 function solution(n) {
     var answer = 0;
     let num = n.toString(2).split('1').length;
